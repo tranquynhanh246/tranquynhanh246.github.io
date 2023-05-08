f3b1ba0a160dd0247687ebea859399602edcac6c
quanghuy1234567 <nguyenquanghuy27042002@gmail.com>
2023-05-08 15:31:38 +0700

    studentDAO class

--- BTTH01/students/Student.php
+++ BTTH01/students/Student.php
@@ -37,3 +37,4 @@ class Student
         $this->age = $age;
     }
 }
+?>
---
+++ BTTH01/students/StudentDAO.php
@@ -0,0 +1,26 @@
+<?php include_once 'Student.php' ;
+    $filename = "../assets/text/student.txt";
+    $f = fopen($filename, "r");
+    $content = fread($f, filesize($filename));
+    $remove = "\n";
+    $arraySt = explode($remove, $content);
+?>
+<?php 
+    class StudentDAO {
+        private $studentArray = [];
+
+        public function createStudent($id, $name, $age){
+            $newST = new Student($id, $name, $age);
+            array_push($studentArray, $newST);
+        }
+
+        public function getAll(){
+            global $arraySt;
+            for ($i=1; $i<count($arraySt); $i++){
+                $infor = explode(",",$arraySt[$i]);
+                array_push($this->studentArray, new Student($infor[0],$infor[1],$infor[2]));
+            }
+            return $this->studentArray;
+        }
+    }
+?>
