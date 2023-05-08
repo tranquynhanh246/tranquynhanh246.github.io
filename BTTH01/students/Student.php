d28991bfbc95675852810bbbd0d2da7c06fb394f
quanghuy1234567 <nguyenquanghuy27042002@gmail.com>
2023-05-08 16:38:16 +0700

    StudentDAO class

---
+++ BTTH01/students/Student.php
@@ -0,0 +1,40 @@
+<?php
+class Student
+{
+    private $id;
+    private $name;
+    private $age;
+
+    public function __construct($id, $name, $age)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+}
+?>
