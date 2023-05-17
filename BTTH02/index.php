6db5d63ace4ae5eda9b06d83d4f657548b51da49
tranqynhanhhh <84771483+tranqynhanhhh@users.noreply.github.com>
2023-05-18 00:48:53 +0700

    index.php

--- BTTH02/index.php
+++ BTTH02/index.php
@@ -2,19 +2,23 @@
 <html>
 <head>
     <title> LOGIN </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h2> LOGIN </h2>
-    <form action="login.php" method="POST">
-        <label for="student_id"> Student ID: </label>
-        <input type="text" id="student_id" name="student_id" required>
-        <br>
-        <br>
-
-        <label for="password"> Password :</label>
-        <input type="password" id="password" name="password" required>
-
-        <input type="submit" value="Submit">
-    </form>
+    <div class="container">
+        <h2> LOGIN </h2>
+        <form method="post" action="process_login.php">
+            <div class="id">
+                <label for="student_id" class="form-label"> Student ID: </label>
+                <input type="text" class="form-control" id="student_id" name="student_id" required>
+            </div>
+            <div class="password">
+                <label for="password" class="form-label"> Password: </label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+        </form>
+    </div>
 </body>
 </html>
+
