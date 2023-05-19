cec6324e0a02d95623ff13bd84dc22ac1d8faf75
virtual-port <hungb.z98@gmail.com>
2023-05-19 12:05:30 +0700

    update admin ver2

--- BTTH02/view/adminCreate.php
+++ BTTH02/view/adminCreate.php
@@ -52,8 +52,8 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fa-solid fa-book"></i>
-                                <p>
-                                    Danh Mục
+                                <p class="ml-50">
+                                    Điểm Danh
                                     <i class="right fas fa-angle-left"></i>
                                 </p>
                             </a>
@@ -68,7 +68,6 @@
                                     <a href="./adminCreate.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Tạo mục điểm danh</p>
-
                                     </a>
                                 </li>
                             </ul>
@@ -106,13 +105,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Quản lý điểm danh</h1>
+                            <h1>Tạo Danh Mục Điểm Danh</h1>
                         </div>
                     </div>
                 </div>
             </section>
 
-            <!-- Main content -->
             <section class="content">
                 <div class="container-fluid">
                     <div class="row">
@@ -174,10 +172,19 @@
                                                 <input type="date" name="date" class="" id="date">
                                             </div>
                                         </div>
+
+                                        <div class="mb-3 row">
+                                            <label for="time" class="col-sm-2 col-form-label">Giờ:</label>
+                                            <div class="col-sm-10">
+                                                <input type="time" id="time" name="time" required>
+                                            </div>
+                                        </div>
+
+
                                     </div>
 
                                     <div class="card-footer">
-                                        <button type="submit" class="btn btn-primary">Xem</button>
+                                        <button type="submit" class="btn btn-primary">Tạo</button>
                                     </div>
                                 </form>
                             </div>
@@ -185,48 +192,6 @@
                     </div>
                 </div>
             </section>
-
-
-            <h1 class="text-center mt-4 mb-5">Bảng Thông Tin</h1>
-
-            <div class="row">
-                <div class="col-md-10 mx-auto">
-                    <table class="table table-bordered table-striped">
-                        <thead class="table-dark">
-                            <tr>
-                                <th>ID</th>
-                                <th>Lớp</th>
-                                <th>Sĩ số</th>
-                                <th>Nghỉ học</th>
-                                <th>Đi học</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>TH1</td>
-                                <td>63</td>
-                                <td>3</td>
-                                <td>60</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>TH2</td>
-                                <td>63</td>
-                                <td>0</td>
-                                <td>63</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>TH3</td>
-                                <td>63</td>
-                                <td>2</td>
-                                <td>61</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
         </div>
 
         <script src="../public/template/admin/plugins/jquery/jquery.min.js"></script>
--- BTTH02/view/clients.php
+++ BTTH02/view/clients.php
@@ -47,15 +47,13 @@
                 </div>
 
 
-
-                <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
-                                    Danh Mục
+                                    Điểm Danh
                                     <i class="right fas fa-angle-left"></i>
                                 </p>
                             </a>
@@ -63,16 +61,22 @@
                                 <li class="nav-item">
                                     <a href="" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
-                                        <p>Điểm Danh</p>
+                                        <p>Khu vực điểm danh 62TH.1</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Khu vực điểm danh 62TH.2</p>
                                     </a>
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </nav>
-                <!-- /.sidebar-menu -->
             </div>
-            <!-- /.sidebar -->
         </aside>
 
         <div class="content-wrapper">
@@ -80,88 +84,11 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Quản lý điểm danh</h1>
+                            <h1>Khu vực điểm danh 62TH2.1</h1>
                         </div>
                     </div>
                 </div>
             </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card card-primary">
-                                <div class="card-header">
-                                    <h3 class="card-title">Thông tin</h3>
-                                </div>
-
-                                <form action="/some-url.php" method="post">
-                                    <div class="card-body">
-                                        <div class="mb-3 row">
-                                            <label for="year" class="col-sm-2 col-form-label">Năm học:</label>
-                                            <div class="col-sm-10">
-                                                <select id="year" name="year" class="form-select">
-                                                    <option selected>Chọn năm học</option>
-                                                    <option value="2021-2022">2021-2022</option>
-                                                    <option value="2022-2023">2022-2023</option>
-                                                    <option value="2023-2024">2023-2024</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="mb-3 row">
-                                            <label for="phase" class="col-sm-2 col-form-label">Học kỳ:</label>
-                                            <div class="col-sm-10">
-                                                <select id="phase" name="phase" class="form-select">
-                                                    <option selected>Chọn học kỳ</option>
-                                                    <option value="Học kỳ 1">Học kỳ 1</option>
-                                                    <option value="Học kỳ 2">Học kỳ 2</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="mb-3 row">
-                                            <label for="stage" class="col-sm-2 col-form-label">Giai đoạn:</label>
-                                            <div class="col-sm-10">
-                                                <select id="stage" name="stage" class="form-select">
-                                                    <option selected>Chọn giai đoạn</option>
-                                                    <option value="Giai đoạn 1">Giai đoạn 1</option>
-                                                    <option value="Giai đoạn 2">Giai đoạn 2</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="mb-3 row">
-                                            <label for="course" class="col-sm-2 col-form-label">Lớp học phần:</label>
-                                            <div class="col-sm-10">
-                                                <select id="course" name="course" class="form-select">
-                                                    <option selected>Chọn lớp học phần</option>
-                                                    <option value="ABC101">Công nghệ Web-2-22</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="mb-3 row">
-                                            <label for="date" class="col-sm-2 col-form-label">Ngày điểm danh:</label>
-                                            <div class="col-sm-10">
-                                                <input type="date" name="date" class="" id="date">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="card-footer">
-                                        <button type="submit" class="btn btn-primary">Điểm danh</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-
-            <h1 class="text-center mt-4 mb-5">Bảng Thông Tin</h1>
 
             <div class="row">
                 <div class="col-md-10 mx-auto">
